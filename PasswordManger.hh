42625ce7ac2b3c23e1c6d5cb3136d69dd9fa5136
DimitarAtanassov <dimitar789558@gmail.com>
2024-03-26 18:52:49 -0700

    Project Setup

---
+++ PasswordManger.hh
@@ -0,0 +1,18 @@
+
+
+#ifndef PASSWORD_MANAGER_H
+#define PASSWORD_MANAGER_H
+#include <map>
+#include <string>
+
+class PasswordManager
+{
+    public:
+
+    private:
+        
+
+}
+
+
+#endif
---
+++ main.cc
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <string>
+#include <limits>
+
+int main (int argc, char* argv[])
+{
+    bool validChoice = false;
+    int userChoice;
+    std::string appCommand;
+    std::cout << "============================" << std::endl;
+    std::cout << "   MyPass     " << std::endl;
+    std::cout << "============================\n" << std::endl;
+
+    while(!validChoice)
+    {   
+        std::cout << "What would you like to do?" << std::endl;
+        std::cout << "1.) Get Password" << std::endl;
+        std::cout <<"2.) Add Password" << std::endl;
+        std::cout <<"3.) Update Password" << std::endl;
+        std::cout <<"4.) Delete Password" << std::endl;
+        std::cout <<"Enter the corresponding number: ";
+        std::cin >> userChoice;
+
+        // Invalid User Input
+        if(std::cin.fail())
+        {
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Discard invalid input
+            std::cout << "Invalid input. Please enter a number 1-4" << std::endl;
+        }
+
+        else
+        {
+            switch(userChoice)
+            {
+                case 1:
+                    appCommand = "Get";
+                    validChoice = true;
+                    break;
+                
+                case 2:
+                    appCommand = "Add";
+                    validChoice = true;
+                    break;
+
+                case 3:
+                    appCommand = "Update";
+                    validChoice = true;
+                    break;
+                
+                case 4:
+                    appCommand = "Delete";
+                    validChoice = true;
+                    break;
+            }
+        }
+    }
+
+    return 0;
+
+}
