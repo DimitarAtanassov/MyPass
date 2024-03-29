27a580dc022f999331ee6418faa1ee2d350196c6
DimitarAtanassov <dimitar789558@gmail.com>
2024-03-29 13:29:44 -0700

    Added File Encryption and Decryption for password security

--- Security.hh
+++ Security.hh
@@ -1,17 +1,25 @@
 #ifndef SECURITY_H
 #define SECURITY_H
-#include string
+#include <string>
 #include <iostream>
-class SECURITY_H
+#include <cryptopp/aes.h>
+#include <cryptopp/modes.h>
+#include <cryptopp/filters.h>
+#include <cryptopp/secblock.h>
+#include <cryptopp/osrng.h>
+#include <cryptopp/sha.h>
+#include <cryptopp/hex.h>
+#include <cryptopp/cryptlib.h>
+class Security
 {
     public:
-        Security(const std::string&)
-        std::string encrypt();
-        std::string decrypt();
-        void encryptFile();
-        void decryptFile();
+        Security();
+        CryptoPP::SecByteBlock GenerateRandomKey(unsigned int keySize);
+        CryptoPP::SecByteBlock DeriveKeyFromPassword(const std::string& password, const CryptoPP::byte* salt, size_t saltSize, unsigned int keySize);
+        void EncryptPasswordFile(const std::string& inputFilename, const std::string& outputFilename, const  CryptoPP::SecByteBlock& key);
+        void DecryptPasswordFile(const std::string& inputFilename, const std::string& outputFilename, const  CryptoPP::SecByteBlock& key);
     private:
-        std::string filePath;
-}
+       
+};
 
 #endif
